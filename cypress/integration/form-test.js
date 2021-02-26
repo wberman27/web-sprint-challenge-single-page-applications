51a5670311bed94d8293d0c57f3dfc6daec7b9cb
Will B <wberman27@gmail.com>
2021-02-26 18:25:34 -0500

    added cypress tests to see if multiple toppings can be checked

--- cypress/integration/form-test.js
+++ cypress/integration/form-test.js
@@ -10,6 +10,10 @@ const textInput = () => cy.get('input[name=name]') //grabbing elements for cypre
 const specInput = () => cy.get('input[name=spec]')
 const sizeSelect = () => cy.get('select[name=size]')
 const submitButton = () => cy.get('#submitButton')
+const pepperCheck = () => cy.get('input[name=pepperoni]')
+const mushCheck = () => cy.get('input[name=mushrooms]')
+const jalaCheck = () => cy.get('input[name=jalapeño]')
+const pineCheck = () => cy.get('input[name=pineapple]')
 
 it('Filling out inputs', () =>{ //check in cypress, if filling out inputs work correctly
     textInput()
@@ -28,5 +32,20 @@ it('Submit button works', () =>{ //checks to see if submit button works
         .click()
     cy.contains('William') //the page contains William after submit button
     
+    
+})
+it('Tests selection of multiple toppings', () =>{
+    pepperCheck()
+        .check()
+        .should('have.value', 'on')
+    mushCheck()
+        .check()
+        .should('have.value', 'on')
+    jalaCheck()
+        .check()
+        .should('have.value', 'on')
+    pineCheck()
+        .check()
+        .should('have.value', 'on')
     
 })
